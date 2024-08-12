efca6e6621d4df3c47e3d11683ec1e1536be40da
jsalicetti <jessicasalicetti@yahoo.com>
2024-08-12 09:28:14 -0400

    course class, null

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -9,12 +9,48 @@ import java.util.Objects;
 import java.util.Set;
 
 /**
- * Course is a POJO, configured as a persistent class that represents (or maps to) a table
- * name 'course' in the database. A Course object contains fields that represent course
- * information and a mapping of 'courses' that indicate an inverse or referencing side
- * of the relationship. Implement Lombok annotations to eliminate boilerplate code.
+ * annotations
  */
 
-public class Course {
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@ToString
+@Setter
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "course")
 
+
+public class Course {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id")
+    int id;
+    @NonNull
+    @Column(length = 50, name = "name")
+    String name;
+    @NonNull
+    @Column(length = 50, name="instructor")
+    String instructor;
+
+    @ToString.Exclude
+    @ManyToMany(mappedBy = "courses", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH},fetch = FetchType.EAGER)
+    private Set<Student> students = new LinkedHashSet<>();
+
+    @Override
+
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id && name.equals(course.name) && instructor.equals(course.instructor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, instructor);
+    }
 }
+
+
